5226af25a0d104c7ad22a3b55ff75c7518f44828
Dima Kolibabchuk <dmitry@kolibabchuk.com>
2026-03-05 10:16:49 +0200

    init

---
+++ overlay/miryoku/mapping/42/hallie.h
@@ -0,0 +1,107 @@
+// Copyright 2022 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+#if !defined (MIRYOKU_LAYOUTMAPPING_HALLIE)
+
+#define XXX &none
+#define HYPR LG(LA(LS(LCTRL)))
+
+// Default mapping for BASE, EXTRA, and TAP layers
+#define MIRYOKU_LAYOUTMAPPING_HALLIE( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+&td_bslh_globe_base        K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  U_MT(GLOBE, LBKT) \
+U_MT(HYPR, RBKT)           K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  U_MT(HYPR, SQT) \
+&mt_input_switch LSHFT 0   K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  U_MT(RSHFT, FSLH) \
+                                K32  K33  K34    K35  K36  K37
+
+// BUTTON layer mapping with custom outer column keys
+#define MIRYOKU_LAYOUTMAPPING_BUTTON( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+U_BOOT K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  U_BOOT \
+U_BOOT K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  U_BOOT \
+U_BOOT K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  U_BOOT \
+                 K32  K33  K34    K35  K36  K37
+
+// NAV layer mapping with custom outer column keys
+#define MIRYOKU_LAYOUTMAPPING_NAV( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+&kp C_AC_DESKTOP_SHOW_ALL_APPLICATIONS  K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp ESC  \
+&kp C_AC_DESKTOP_SHOW_ALL_WINDOWS       K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp HYPR \
+&kp C_AC_SEARCH                         K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp GLOBE \
+                                                  K32  K33  K34    K35  K36  K37
+
+// MOUSE layer mapping with custom outer column keys
+#define MIRYOKU_LAYOUTMAPPING_MOUSE( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+&kp C_AC_DESKTOP_SHOW_ALL_APPLICATIONS  K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp ESC \
+&kp C_AC_DESKTOP_SHOW_ALL_WINDOWS       K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp HYPR \
+&kp C_AC_SEARCH                         K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp GLOBE \
+                                                  K32  K33  K34    K35  K36  K37
+
+// MEDIA layer mapping with custom outer column keys
+#define MIRYOKU_LAYOUTMAPPING_MEDIA( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+&kp C_AC_DESKTOP_SHOW_ALL_APPLICATIONS  K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp ESC \
+&kp C_AC_DESKTOP_SHOW_ALL_WINDOWS       K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp HYPR \
+&kp C_AC_SEARCH                         K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp GLOBE \
+                                                  K32  K33  K34    K35  K36  K37
+
+// NUM layer mapping with custom outer column keys
+#define MIRYOKU_LAYOUTMAPPING_NUM( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+&kp C_AC_DESKTOP_SHOW_ALL_APPLICATIONS  K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp ESC \
+&kp C_AC_DESKTOP_SHOW_ALL_WINDOWS       K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp HYPR \
+&kp C_AC_SEARCH                         K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp GLOBE \
+                                                  K32  K33  K34    K35  K36  K37
+
+// SYM layer mapping with custom outer column keys and unicode tilde/backtick
+#define MIRYOKU_LAYOUTMAPPING_SYM( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+&kp C_AC_DESKTOP_SHOW_ALL_APPLICATIONS  K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp ESC \
+&kp C_AC_DESKTOP_SHOW_ALL_WINDOWS       K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp HYPR \
+&kp C_AC_SEARCH                         K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp GLOBE \
+                                                  K32  K33  K34    K35  K36  K37
+
+// FUN layer mapping with numbers 1-6 on outer columns
+#define MIRYOKU_LAYOUTMAPPING_FUN( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+&kp C_AC_DESKTOP_SHOW_ALL_APPLICATIONS  K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp ESC \
+&kp C_AC_DESKTOP_SHOW_ALL_WINDOWS       K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp HYPR \
+&kp C_AC_SEARCH                         K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp GLOBE \
+                                                  K32  K33  K34    K35  K36  K37
+
+#endif
+
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_HALLIE
---
+++ overlay/miryoku/miryoku_babel/miryoku_layer_list.h
@@ -0,0 +1,32 @@
+// Copyright 2022 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+// generated -*- buffer-read-only: t -*-
+
+#pragma once
+
+#if !defined (MIRYOKU_LAYER_LIST)
+
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(BASE,   "Base",   sensor-bindings = < &inc_dec_kp LEFT RIGHT &inc_dec_kp UP DOWN> ;) \
+MIRYOKU_X(EXTRA,  "Extra",  sensor-bindings = < &inc_dec_kp LEFT RIGHT &inc_dec_kp UP DOWN> ;) \
+MIRYOKU_X(TAP,    "Tap",    sensor-bindings = < &inc_dec_kp LEFT RIGHT &inc_dec_kp UP DOWN> ;) \
+MIRYOKU_X(BUTTON, "Button", sensor-bindings = < &inc_dec_kp LEFT RIGHT &inc_dec_kp UP DOWN> ;) \
+MIRYOKU_X(NAV,    "Nav",    sensor-bindings = < &inc_dec_kp UP DOWN &inc_dec_kp LEFT RIGHT> ;) \
+MIRYOKU_X(MOUSE,  "Mouse",  sensor-bindings = < &inc_dec_kp LEFT RIGHT &inc_dec_kp UP DOWN> ;) \
+MIRYOKU_X(MEDIA,  "Media",  sensor-bindings = <&inc_dec_kp C_BRI_DEC C_BRI_INC &inc_dec_kp C_VOL_UP C_VOL_DN> ;) \
+MIRYOKU_X(NUM,    "Num",    sensor-bindings = < &inc_dec_kp LEFT RIGHT &inc_dec_kp UP DOWN> ;) \
+MIRYOKU_X(SYM,    "Sym",    sensor-bindings = < &inc_dec_kp LEFT RIGHT &inc_dec_kp UP DOWN> ;) \
+MIRYOKU_X(FUN,    "Fun",    sensor-bindings = < &inc_dec_kp LEFT RIGHT &inc_dec_kp UP DOWN> ;)
+
+#define U_BASE   0
+#define U_EXTRA  1
+#define U_TAP    2
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MOUSE  5
+#define U_MEDIA  6
+#define U_NUM    7
+#define U_SYM    8
+#define U_FUN    9
+
+#endif
